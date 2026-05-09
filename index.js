0b6ff9222fced8a4011ef213976654f88a4c6766
Eich <lucas.eich@saipos.com>
2026-05-09 00:54:44 -0300

    G7 - JSON

--- index.js
+++ index.js
@@ -1,32 +1,20 @@
-var name = document.querySelector("#exampleInputName");
-var gender = document.querySelectorAll("#form-user-create [name=gender]:checked");
-var birth = document.querySelector("#exampleInputBirth");
-var country = document.querySelector("#exampleInputCountry");
-var email = document.querySelector("#exampleInputEmail");
-var password = document.querySelector("#exampleInputPassword");
-var photo = document.querySelector("#exampleInputFile");
-var admin = document.querySelector("#exampleInputAdmin");
-
-
-
-
 var fields = document.querySelectorAll("#form-user-create [name]");
+var user = {};
 
 fields.forEach(function(field, index){
 
     if (field.name == "gender") {
 
         if (field.checked === true) {
-            console.log("SIM", field);
+            user[field.name] = field.value;
         }
-
 
     } else {
 
-        console.log("NÃO");
+        user[field.name] = field.value;
 
     }
 
-        //console.log(, field.id, field.value, field.checked, index);
-
 });
+
+console.log(user);
