6d6745af72ea5d012f2e8ac5f01ada373f426f18
Eich <lucas.eich@saipos.com>
2026-05-09 02:51:35 -0300

    G8 - Eventos em JS

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 var fields = document.querySelectorAll("#form-user-create [name]");
-var user = {};
+var user = {}; 
 
-fields.forEach(function(field, index){
+document.getElementById("form-user-create").addEventListener("submit", function(event){
 
-    if (field.name == "gender") {
+    event.preventDefault();
 
-        if (field.checked === true) {
-            user[field.name] = field.value;
+    fields.forEach(function(field, index){
+
+        if (field.name === "gender") {
+
+            if (field.checked) {
+                user[field.name] = field.value
+            }
+
+        } else {
+
+            user[field.name] = field.value
+
         }
 
-    } else {
+    });
 
-        user[field.name] = field.value;
-
-    }
+    console.log(user);
 
 });
-
-console.log(user);
