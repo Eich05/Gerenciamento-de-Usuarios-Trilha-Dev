1dbb7eee34e5a8be2f3139da47a2aeac98841952
Eich <lucas.eich@saipos.com>
2026-05-09 21:49:21 -0300

    G21 - Configurando botão Editar

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -6,6 +6,17 @@ class UserController {
         this.tableEl = document.getElementById(tableId);
 
         this.onSubmit()
+        this.onEdit();
+
+    }
+
+    onEdit(){
+
+        document.querySelector("#box-user-update .btn-cancel").addEventListener("click", e => {
+
+            this.showPanelCreate();
+
+        });
 
     }
 
@@ -17,11 +28,11 @@ class UserController {
 
             let btn = this.formEl.querySelector("[type=submit]");
 
-            btn.disabled = true;
-
             let values = this.getValues();
 
-            if (!values) return false;            
+            if (!values) return false;
+            
+            btn.disabled = true;
             
             this.getPhoto().then(
                 (content) => {
@@ -145,15 +156,36 @@ class UserController {
                 <td>${(dataUser.admin) ? 'Sim' : 'Não'}</td>
                 <td>${Utils.dateFormat(dataUser.register)}</td>
                 <td>
-                    <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
-                    <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+                    <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
+                    <button type="button" class="btn btn-danger btn-delete btn-xs btn-flat">Excluir</button>
                 </td>
             </tr>
         `;
 
+        tr.querySelector(".btn-edit").addEventListener("click", e => {
+
+            console.log(JSON.parse(tr.dataset.user));
+            this.showPanelUpdate();
+
+        });
+
         this.tableEl.appendChild(tr);
 
         this.updateCount();
+
+    }
+
+    showPanelCreate(){
+
+        document.querySelector("#box-user-create").style.display = "block";
+        document.querySelector("#box-user-update").style.display = "none";
+
+    }
+
+    showPanelUpdate(){
+
+        document.querySelector("#box-user-create").style.display = "none";
+        document.querySelector("#box-user-update").style.display = "block";
 
     }
 
